47ee1631926098fafb3435bd2dd7d286444dc648
Aspen James <aspenjames@tqca.org>
2018-02-06 22:39:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ function createRock(x) {
       endGame();
     } else {
       top = top + 2;
+      rock.style.top = `${top}px`
       if(top<380){
         window.requestAnimationFrame(moveRock);
       } else {
